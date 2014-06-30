ba450c2cf4c23013cc8c1b32646856547b028689
Jake Bernstein <jacobjbernstein@gmail.com>
2014-06-30 03:25:48 +0000

    First Commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var app = express();
 
+var fs=require('fs');
+
+var data = fs.readFileSync("index.html", "utf-8");
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send(data);
 })
 
 app.listen(app.get('port'), function() {
